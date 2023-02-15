af812fc3ae83a3d521715f5c08d94f4a3e5ac806
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-02-15 18:15:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+// lets get the button and the input field and parent OL list so child can be added
+var button = document.getElementById('addTodoBtn');
+var input = document.getElementById('newTodoInput');
+var parentList = document.getElementById('todoList');
+
+function appendToList(){
+	var todo = document.createElement('li');
+	todo.textContent = input.value;
+	parentList.appendChild(todo);
+}
+// add event listener to button
+button.addEventListener('click', appendToList)
+
+// if the button is clicked, append child to parent
+
+
 
